ed46ddc0a97e50076ff29b1493da0a898329cf2c
Ravi <raviteja.valluri@rackspace.com>
2020-10-05 11:53:04 -0700

    Netlify Plugin for URL Check

--- plugins/netlify-plugin-check-urls/index.js
+++ plugins/netlify-plugin-check-urls/index.js
@@ -1,25 +1,6 @@
 const { exec } = require('child_process');
 module.exports = {
     onPostBuild: () => {
-        const grep = exec(`grep -Eoi '<a [^>]+>' public/**/*.html |
-        grep -Eo 'href="[^\"]+"' | 
-        grep -Eo '(http|https)://[^/"]+' > urls.txt |
-        go run checkUrls/checkUrls.go urls.txt`, function (error, stdout, stderr) {
-            if (error) {
-              console.log(error.stack);
-              console.log('Error code: '+error.code);
-              console.log('Signal received: '+error.signal);
-            }
-            console.log('Child Process STDOUT: '+stdout);
-            console.log('Child Process STDERR: '+stderr);
-          });
-          grep.on('exit', function (code) {
-            console.log('Child process exited with exit code '+code);
-          });
-        // exec(`
-        // grep -Eoi '<a [^>]+>' public/**/*.html |
-        // grep -Eo 'href="[^\"]+"' | 
-        // grep -Eo '(http|https)://[^/"]+' > urls.txt |
-        // go run checkUrls / checkUrls.go urls.txt`);
+    console.log('Adding Link checker');
     }
 }
