782ef8186d6ab89afa4b5eb78ef5e1693b12203c
Ravi <raviteja.valluri@rackspace.com>
2020-11-04 07:04:36 -0800

    New Crawler Plugin Install/Uninstall

--- src/js/app.js
+++ src/js/app.js
@@ -8,162 +8,153 @@ contentLoaded().then(() => {
    * Enable search
    */
   try {
-    // let lastRenderArgs;
-    // let renderHTML = ``;
     algoliasearchNetlify({
-      appId: ALGOLIA_APP_ID,
-      apiKey: ALGOLIA_API_KEY,
-      siteId: ALGOLIA_SITE_ID,
-      branch: ALGOLIA_NETLIFY_BRANCH,
+      appId: '56HNYY0XCO',
+      apiKey: '5e0e3f031ab81ebbe105dbf6e5dfd5fd',
+      siteId: '72d02424-c8c6-4745-9cd5-b90dcb09b67e',
+      branch: 'staging',
     });
-    // const infiniteHits = instantsearch.connectors.connectInfiniteHits(
-    //   (renderArgs, isFirstRender) => {
-    //     const {
-    //       hits,
-    //       showMore,
-    //       widgetParams
-    //     } = renderArgs;
-    //     const {
-    //       container
-    //     } = widgetParams;
-    //     lastRenderArgs = renderArgs;
-    //     if (isFirstRender) {
-    //       const sentinel = document.createElement('div');
-    //       container.appendChild(document.createElement('ul'));
-    //       container.appendChild(sentinel);
-    //       const observer = new IntersectionObserver(entries => {
-    //         entries.forEach(entry => {
-    //           if (entry.isIntersecting && !lastRenderArgs.isLastPage) {
-    //             showMore();
-    //           }
-    //         });
-    //       });
-    //       observer.observe(sentinel);
-    //       return;
-    //     }
-    //     container.querySelector('ul').innerHTML = hits
-    //       .map(
-    //         (hit) => {
-    //           const entities = new Entities();
-    //           if (hit.product_url != null && hit.created_by != null && hit.created_by != '' && hit.created_date != null && !hit.permalink.includes('all-articles')) {
-    //             renderHTML = `<li class="hit-item-single">
-    //               <div class="row">
-    //                 <div class="col-sm-12">
-    //                   <p class="search-product">
-    //                     <a class="search-product-link" href="/support/how-to/">How-To</a> &nbsp; > &nbsp; <a class="search-product-link" href=/support/how-to/${hit.product_url}>${hit.product}</a>
-    //                   </p> 
-    //                   <h2>
-    //                     <a class="search-title" href=/support/how-to/${hit.permalink}>${instantsearch.highlight({ attribute: 'title', hit })}</a>
-    //                   </h2>
-    //                   <a class="search-summary-link" href=/support/how-to/${hit.permalink}>
-    //                     <p class="search-summary">${instantsearch.highlight({ attribute: 'content', hit })}</p>
-    //                   </a>
-    //                   <span class="search-author" > By &nbsp; ${instantsearch.highlight({ attribute: 'created_by', hit })}</span>
-    //                   <span class="search-date">${hit.last_modified_date}</span> 
-    //               </div>
-    //             </div>
-    //           </li>`;
-    //           } else {
-    //             renderHTML = `<span></span>`
-    //           }
-    //           return entities.decode(renderHTML);
-    //         }
-    //       )
-    //       .join('');
-    //   }
-    // );
-    // const renderStats = (renderOptions, isFirstRender) => {
-    //   const {
-    //     nbHits,
-    //     widgetParams
-    //   } = renderOptions;
+    let lastRenderArgs;
+    let renderHTML = ``;
+    const infiniteHits = instantsearch.connectors.connectInfiniteHits(
+      (renderArgs, isFirstRender) => {
+        const {
+          hits,
+          showMore,
+          widgetParams
+        } = renderArgs;
+        const {
+          container
+        } = widgetParams;
+        lastRenderArgs = renderArgs;
+        if (isFirstRender) {
+          const sentinel = document.createElement('div');
+          container.appendChild(document.createElement('ul'));
+          container.appendChild(sentinel);
+          const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+              if (entry.isIntersecting && !lastRenderArgs.isLastPage) {
+                showMore();
+              }
+            });
+          });
+          observer.observe(sentinel);
+          return;
+        }
+        container.querySelector('ul').innerHTML = hits
+          .map(
+            (hit) => {
+              const entities = new Entities();
+              if (hit.product_url != null && hit.created_by != null && hit.created_by != '' && hit.created_date != null && !hit.permalink.includes('all-articles')) {
+                renderHTML = `<li class="hit-item-single">
+                  <div class="row">
+                    <div class="col-sm-12">
+                      <p class="search-product">
+                        <a class="search-product-link" href="/support/how-to/">How-To</a> &nbsp; > &nbsp; <a class="search-product-link" href=/support/how-to/${hit.product_url}>${hit.product}</a>
+                      </p> 
+                      <h2>
+                        <a class="search-title" href=/support/how-to/${hit.permalink}>${instantsearch.highlight({ attribute: 'title', hit })}</a>
+                      </h2>
+                      <a class="search-summary-link" href=/support/how-to/${hit.permalink}>
+                        <p class="search-summary">${instantsearch.highlight({ attribute: 'content', hit })}</p>
+                      </a>
+                      <span class="search-author" > By &nbsp; ${instantsearch.highlight({ attribute: 'created_by', hit })}</span>
+                      <span class="search-date">${hit.last_modified_date}</span> 
+                  </div>
+                </div>
+              </li>`;
+              } else {
+                renderHTML = `<span></span>`
+              }
+              return entities.decode(renderHTML);
+            }
+          )
+          .join('');
+      }
+    );
+    const renderStats = (renderOptions, isFirstRender) => {
+      const {
+        nbHits,
+        widgetParams
+      } = renderOptions;
+      if (isFirstRender) {
+        return;
+      }
 
-    //   if (isFirstRender) {
-    //     return;
-    //   }
-
-    //   let count = '';
-    //   if (nbHits > 1) {
-    //     count += `<p class="results-p">${nbHits} results found</p>`;
-    //   } else if (nbHits === 1) {
-    //     count += `<p>1 result found</p>`;
-    //   } else {
-    //     count += `<p class="no-results-p1">No results found</p>
-    //     <p class="no-results-p2">It seems we can’t find any results based on your search.</p>`;
-    //   }
-    //   widgetParams.container.innerHTML = `${count}`;
-    // };
-    // const algoliaClient = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_SEARCH_KEY);
-    // const searchClient = {
-    //   search(requests) {
-    //     if (requests.every(({
-    //         params
-    //       }) => !params.query)) {
-    //       return Promise.resolve({
-    //         results: requests.map(() => ({
-    //           hits: [],
-    //           nbHits: 0,
-    //           nbPages: 0,
-    //         })),
-    //       });
-    //     }
-    //     return algoliaClient.search(requests);
-    //   },
-    // };
+      let count = '';
+      if (nbHits > 1) {
+        count += `<p class="results-p">${nbHits} results found</p>`;
+      } else if (nbHits === 1) {
+        count += `<p>1 result found</p>`;
+      } else {
+        count += `<p class="no-results-p1">No results found</p>
+        <p class="no-results-p2">It seems we can’t find any results based on your search.</p>`;
+      }
+      widgetParams.container.innerHTML = `${count}`;
+    };
+    const algoliaClient = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_API_KEY);
+    const searchClient = {
+      search(requests) {
+        if (requests.every(({
+            params
+          }) => !params.query)) {
+          return Promise.resolve({
+            results: requests.map(() => ({
+              hits: [],
+              nbHits: 0,
+              nbPages: 0,
+            })),
+          });
+        }
+        return algoliaClient.search(requests);
+      },
+    };
     // Create the custom widget
-    // const customStats = instantsearch.connectors.connectStats(renderStats);
-    // const search = instantsearch({
-    //   indexName: ALGOLIA_PUBLIC_INDEX,
-    //   searchClient: searchClient,
-    //   searchFunction(helper) {
-    //     const hitsContainer = document.querySelector('#hits');
-    //     const paginationContainer = document.querySelector('#pagination');
-    //     const statsContainer = document.querySelector('#stats');
-    //     hitsContainer.style.display = helper.state.query === '' ? 'none' : '';
-        
-    //     paginationContainer.style.display = helper.state.query === '' ? 'none' : '';
-    //     statsContainer.style.display = helper.state.query === '' ? 'none' : '';
-    //     helper.search();
-    //   },
-    // });
-    // search.addWidgets([
-    //   instantsearch.widgets.searchBox({
-    //     container: '#searchbox',
-    //     placeholder: '',
-    //     autofocus: true,
-    //     showLoadingIndicator: true,
-    //     searchAsYouType: true,
-    //     wrapInput: false,
-    //     magnifier: false,
-    //     reset: false,
-    //     poweredBy: false,
-    //     escapeHTML: true
-    //   }),
-    //   instantsearch.widgets.configure({
-    //     attributesToHighlight: [
-    //       'content:160',
-    //       'title',
-    //       'author'
-    //     ],
-    //     attributesToRetrieve: [
-    //       '*'
-    //     ]
-    //   }),
-      // instantsearch.widgets.index({
-      //   indexName: AlGOLIA_SUPPORT_INDEX
-      // }).addWidgets([
-      //   supportInfiniteHits({
-      //     container: document.querySelector('#support-hits')
-      //   }),
-      // ]),
-    //   infiniteHits({
-    //     container: document.querySelector('#hits')
-    //   }),
-    //   customStats({
-    //     container: document.querySelector('#stats'),
-    //   })
-    // ]);
-    // search.start();
+    const customStats = instantsearch.connectors.connectStats(renderStats);
+    const search = instantsearch({
+      indexName: ALGOLIA_SUPPORT_INDEX,
+      searchClient: searchClient,
+      searchFunction(helper) {
+        const hitsContainer = document.querySelector('#hits');
+        const paginationContainer = document.querySelector('#pagination');
+        const statsContainer = document.querySelector('#stats');
+        hitsContainer.style.display = helper.state.query === '' ? 'none' : '';
+        paginationContainer.style.display = helper.state.query === '' ? 'none' : '';
+        statsContainer.style.display = helper.state.query === '' ? 'none' : '';
+        helper.search();
+      },
+    });
+    search.addWidgets([
+      instantsearch.widgets.searchBox({
+        container: '#searchbox',
+        placeholder: '',
+        autofocus: true,
+        showLoadingIndicator: true,
+        searchAsYouType: true,
+        wrapInput: false,
+        magnifier: false,
+        reset: false,
+        poweredBy: false,
+        escapeHTML: true
+      }),
+      instantsearch.widgets.configure({
+        attributesToHighlight: [
+          'content:160',
+          'title',
+          'author'
+        ],
+        attributesToRetrieve: [
+          '*'
+        ]
+      }),
+      infiniteHits({
+        container: document.querySelector('#hits')
+      }),
+      customStats({
+        container: document.querySelector('#stats'),
+      })
+    ]);
+    search.start();
   } catch (err) {
     console.warn(err)
   }
--- webpack.config.js
+++ webpack.config.js
@@ -42,6 +42,7 @@ module.exports = {
       ALGOLIA_APP_ID: JSON.stringify(process.env.ALGOLIA_APP_ID),
       ALGOLIA_API_KEY: JSON.stringify(process.env.ALGOLIA_API_KEY),
       ALGOLIA_SITE_ID: JSON.stringify(process.env.ALGOLIA_SITE_ID),
-      ALGOLIA_NETLIFY_BRANCH: JSON.stringify(process.env.ALGOLIA_NETLIFY_BRANCH)
+      ALGOLIA_NETLIFY_BRANCH: JSON.stringify(process.env.ALGOLIA_NETLIFY_BRANCH),
+      ALGOLIA_SUPPORT_INDEX: JSON.stringify(process.env.ALGOLIA_SUPPORT_INDEX)
     })],
 };
